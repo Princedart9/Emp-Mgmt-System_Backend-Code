3ea7e8ac74314faba3cbccad67bffb0c3efb2773
Princedart9 <princedart9d@gmail.com>
2023-06-25 16:36:11 +0530

    Committing to the exp Package!!

---
+++ src/main/java/com/example/springboot/experiment/FirstExp.java
@@ -0,0 +1,13 @@
+package com.example.springboot.experiment;
+
+public class FirstExp {
+
+	public FirstExp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Testing...........");
+	}
+
+}
